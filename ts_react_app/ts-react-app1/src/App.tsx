1f9b1c5781fea2fd9650394deb572163957178f7
sdsanchezm <sdsanchezm@gmail.com>
2023-03-08 00:23:09 -0500

    3 types of props

--- ts_react_app/ts-react-app1/src/App.tsx
+++ ts_react_app/ts-react-app1/src/App.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter';
+import CounterRegularProp from './components/CounterRegularProp';
+import CounterType from './components/CounterType';
+import CounterInterface from './components/CounterInterface';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React qweqewasdasd test
-        </a>
-        <button></button>
-        <Counter />
+        <button>click here</button>
+        <CounterRegularProp number="jamecho!"/>
+        <CounterType number="tiche!"/>
+        <CounterInterface number="Jara!"/>
       </header>
     </div>
   );
